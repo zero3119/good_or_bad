1f7747d3b5a83681ad9b3d2c051597fdf70b15a1
Adan Zamorano <azamorano@miners.utep.edu>
2025-06-27 13:05:38 +0000

    Imported everything necesary to make the engine run, created the sql table, and now showcases the information

--- good_or_bad.py
+++ good_or_bad.py
@@ -1,4 +1,7 @@
+import json
 import requests
+import pandas as pd
+import sqlalchemy as db
 
 APIKEY = ""
 title = input("What movie rating would you like to know? ")
@@ -6,7 +9,18 @@ url = "https://www.omdbapi.com"
 
 params = {'apikey': APIKEY, 't': title}
 
-
 response = requests.get(url , params=params)
 data = response.json()
-print(data["Title"], data["imdbRating"])
+
+data.pop("Ratings", None)
+
+df = pd.DataFrame([data])
+
+engine = db.create_engine('sqlite:///data_base_name.db')
+df.to_sql('table_name', con=engine, if_exists='replace', index=False)
+
+with engine.connect() as connection:
+   query_result = connection.execute(db.text("SELECT * FROM table_name;")).fetchall()
+   print(pd.DataFrame(query_result))
+
+# print(data["Title"], data["imdbRating"])
